b0109c699c52d1d394f72d1b48ecf0e6c750ea37
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2023-11-23 22:48:56 +0545

    dynamic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+// import chakraTheme from "@chakra-ui/theme";
+
+// const { Button } = chakraTheme.components;
+
+// const theme = extendBaseTheme({});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
